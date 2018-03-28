1ad8c77738dc7a241769927d5894281c43ac3bf9
fabio <fx@espa.unisalento.it>
2018-03-28 09:24:18 +0200

    add player

--- vueProject/src/store.js
+++ vueProject/src/store.js
@@ -4,13 +4,25 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    state: {
+        players: {}
+    },
+    mutations: {
+        addPlayer(state, player) {
+            state.players[player.doc_id] = player.player;
+        }
+    },
+    actions: {
+        addPlayer(context, player) {
+            return fetch("http://localhost:5000/data/players", {
+                method: "POST",
+                body: JSON.stringify(player)
+            }).then((res) => res.json()).then(function (res) {
+                if (res.results === undefined)
+                    return Promise.reject(res.error);
+                context.commit("addPlayer", res.results);
+                return Promise.resolve(res.results);
+            }).catch((e) => Promise.reject(e))
+        }
+    }
 })
